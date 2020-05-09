92a4a5cb8f2dfbba9b2ecf534d34d4c594db4c55
cbasJS <sebas.martha@gmail.com>
2020-05-09 13:22:33 -0500

    Add script for branch master

--- travis/bin/deploy.sh
+++ travis/bin/deploy.sh
@@ -3,10 +3,10 @@
 if [[ $TRAVIS_BRANCH = "master" ]]
 then
   while sleep 60s; do echo "Waiting. Time elapsed: $SECONDS seconds"; done &
-  yarn run deploy:prod
+  STAGE=prod AWS_S3_BUCKET=$AWS_S3_BUCKET AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID yarn deploy
   kill %1
 else
   while sleep 60s; do echo "Waiting. Time elapsed: $SECONDS seconds"; done &
-  STAGE=dev AWS_S3_BUCKET=$AWS_S3_BUCKET AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID yarn deploy
+  AWS_S3_BUCKET=$AWS_S3_BUCKET AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID yarn deploy
   kill %1
 fi
